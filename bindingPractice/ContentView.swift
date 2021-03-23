6590f5d5abba7bd65ff0ff2605608c0c6ab71d73
Terry <terrykuo501@gmail.com>
2021-03-23 15:36:05 +0800

    Initial Commit

---
+++ bindingPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bindingPractice
+//
+//  Created by Terry Kuo on 2021/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
